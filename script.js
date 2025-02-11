6eca59346a74927c9546d2dc4dc9d42a2b6031dc
bots123-ux <rezdelacruz6@gmail.com>
2025-02-11 23:00:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('surpriseButton').addEventListener('click', function() {
+    const surpriseMessage = document.getElementById('surpriseMessage');
+    surpriseMessage.style.display = 'block'; // Show the surprise message
+});
