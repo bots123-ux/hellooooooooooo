d5a710818d80e6e7e0c6a9f03223b0b18ef3b892
bots123-ux <rezdelacruz6@gmail.com>
2025-02-11 23:00:22 +0800

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// script.js
-document.getElementById('surpriseButton').addEventListener('click', function() {
-    const surpriseMessage = document.getElementById('surpriseMessage');
-    surpriseMessage.style.display = 'block'; // Show the surprise message
-});
